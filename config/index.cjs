b7c880ec2d5e27a0c514b43781a968c6a5d11bfb
JIULEEE <128106873+JIULEEE@users.noreply.github.com>
2024-04-07 22:59:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,8 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyX_D6ka9G9TikQ9qcaSHPucN79w',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'bCdRM8eBzwY_9Y7K6YFFAD-lWuHRjpNQfDqQDYxS7yU',
+      // useTemplateId: 'bCdRM8eBzwY_9Y7K6YFFAD-lWuHRjpNQfDqQDYxS7yU',
+      useTemplateId: 'NQDONDfAkMNVHInAngB2cFNOTKDMgwtDxPh6qw4hc5M',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-28',
       festivals: [
