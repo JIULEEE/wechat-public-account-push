210e4db1db573ffebdc4de8b1dc5871cf73a7d7f
JIULEEE <128106873+JIULEEE@users.noreply.github.com>
2024-04-07 21:17:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyX_D6ugUa0Zc8mAFlu0tg81Y01g',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'z1IrC1tfXlqUFoBMYKN0y7Y5s8ITlZDohO2GxyGQdSs',
+      useTemplateId: 'y0FsLzjnUl3YLMciw8Rqse6sf8brcghAfXLUMw9KHGg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '13-28',
       festivals: [
@@ -44,7 +44,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyX_D6ka9G9TikQ9qcaSHPucN79w',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'z1IrC1tfXlqUFoBMYKN0y7Y5s8ITlZDohO2GxyGQdSs',
+      useTemplateId: 'y0FsLzjnUl3YLMciw8Rqse6sf8brcghAfXLUMw9KHGg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '13-28',
       festivals: [
